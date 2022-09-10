49588b9a92c051ca394d76063a5d745eb6ae2f90
saraygonm <111905625+saraygonm@users.noreply.github.com>
2022-09-09 23:35:40 -0500

    Add files via upload

---
+++ vectores/prueba_libreriavect.py
@@ -0,0 +1,16 @@
+#RUEBA DE LIBRERIA OPERACIONES CON NUMEROS COMPLEJOS
+#desarrollado por: Saray Alieth Mendivelso Gonzalez
+
+import unittest
+import math
+import libreria_vectores as lcp
+
+class Test_libcplx(unittest.TestCase):
+
+    def test_suma_vectores8(self):
+        #vec1,vec2 = [1,2],[3,4]
+        self.assertEqual(lcp.suma_vectores([4+5j,2+3j],[2+3j,1+2j]), [(6+8j), (3+5j)])
+             
+if __name__ == '__main__':
+    unittest.main()
+

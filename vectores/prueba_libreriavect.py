18354fe0083aef327075bf03e0af8b6d60d6324d
saraygonm <111905625+saraygonm@users.noreply.github.com>
2022-09-10 20:39:17 -0500

    Update prueba_libreriavect.py

--- vectores/prueba_libreriavect.py
+++ vectores/prueba_libreriavect.py
@@ -10,6 +10,7 @@ class Test_libcplx(unittest.TestCase):
     def test_suma_vectores8(self):
         #vec1,vec2 = [1,2],[3,4]
         self.assertEqual(lcp.suma_vectores([4+5j,2+3j],[2+3j,1+2j]), [(6+8j), (3+5j)])
+    def testinvers()
              
 if __name__ == '__main__':
     unittest.main()

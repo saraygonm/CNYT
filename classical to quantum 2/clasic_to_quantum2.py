ed42cf279cdfc44bf2a97fad3927e8c33bc8f643
saraygonm <111905625+saraygonm@users.noreply.github.com>
2022-09-19 00:57:09 -0500

    Update clasic_to_quantum2.py

--- classical to quantum 2/clasic_to_quantum2.py
+++ classical to quantum 2/clasic_to_quantum2.py
@@ -1,42 +1,58 @@
-import matplotlib.pyplot as plt
-import libreria_vectores as nc
+from libreria_vectores import *
+import libcplx as lc
+import matplotlib.pyplot as plot
+import numpy as np
 
 
-# Los experimentos de la canicas con coeficiente booleanos (canicas)
-# Experimentos de las múltiples rendijas clásico probabilístico, con más de dos rendijas. (probabilistico)
-# Experimento de las múltiples rendijas cuántico. (cuantico)
-# Cree una función para graficar con un diagrama de barras que muestre las probabilidades de un vector de estados. La imagen se debe poder guardar en el computador con un formato de imagen. (barras)
 
-def canicas(m, e, r):
-    re = [0 for i in range(len(m))]
-    for i in range(len(m)):
-        for j in range(len(m[0])):
-            re[i] = re[i] + (m[i][j] * e[j])
-    return canicas(m, re, r - 1) if r > 0 else re
+def ult_mati(mati):
+    fila, columna = len( mati ), len( mati[0] )
+    for i in range(fila):
+        new = []
+        for j in range(columna):
+            new.append( [( lc.modulo( mati[i][j] )**2 ),0])   
+        mati[i]= new
+    return mati
 
 
-def propabilistico(m, e, r):
-    re = [0 for i in range(len(m))]
-    for i in range(len(m)):
-        for j in range(len(m[0])):
-            re[i] = re[i] + (m[i][j] * e[j])
-    return propabilistico(m, re, r - 1) if r > 0 else re
+def sistemaprobabilistico_comple(mati, vec, veces):
+    if (veces >= 0) and (type(veces) is int):
+        tamaño  = len(vec)
+        #copia matriz
+        igual = mati[:]
+        for i in range(veces ):
+            mati = multiplicacion(mati, igual)
+        return ult_mati(mati)
+    return -1
 
 
-def cuantico(m, e, r):
-    re = [(0, 0) for i in range(len(m))]
-    for i in range(len(m)):
-        for j in range(len(m[0])):
-            re[i] = nc.suma_vectores(re[i], (nc.multiplicacion(m[i][j], e[j])))
-    return propabilistico(m, re, r - 1) if r > 0 else re
+def sistemaprobabilistico(mati, vec, veces):
+    if (veces >= 0) and (type(veces) is int):
+        tamaño = len(vec)
+        for i in range(veces):
+            vec = accion(mati, vec)
+        return vec
+    return -1
 
+#experimento matriz booleana   
+def experi_bolean_mat(veces,mat,vec):
+    if (veces >= 0 and type(veces) is int):
+        for rep in range(veces):
+            vec = accion(mat, vec)      
+        return vec
 
-def barras(eje_x, eje_y, z):
-    plt.bar(eje_x, eje_y, color="Green")
-    plt.ylabel('Probabilidad')
-    plt.xlabel('Estado')
-    plt.title(z)
-    plt.show()
+def multipleslitexperiment_cuantico(mati , vec, veces ):
+    return sistemaprobabilistico_comple(mati , vec, veces )       
+
+def multipleslitexperimento(mati,vec,veces):
+        return  sistemaprobabilistico(mati,vec,veces)
     
-if __name__ == '__main__':
-    barras(["x", "y", "z"],propabilistico([[0, 1 / 6, 5 / 6], [1 / 3, 1 / 2, 1 / 3], [2 / 3, 1 / 3, 0]], [1, 0, 0], 1)," Sistema Probabilistico")
+
+def grafico(vector):
+    data = len(vector)
+    x = np.array([ x for x in range( data )])
+    y = np.array([ round(vector[x][0]*100,2) for x in range( data )])
+
+    plot.bar(x,y, color ='g', align='centro')
+    plot.title('Probabilidades del vector')
+    plot.show()

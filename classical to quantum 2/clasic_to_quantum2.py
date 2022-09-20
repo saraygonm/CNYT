b0802f8f2781f322ae19f4128b23852dc4a2f9d3
saraygonm <111905625+saraygonm@users.noreply.github.com>
2022-09-20 12:44:41 -0500

    Update clasic_to_quantum2.py

--- classical to quantum 2/clasic_to_quantum2.py
+++ classical to quantum 2/clasic_to_quantum2.py
@@ -7,11 +7,11 @@ import numpy as np
 
 def ult_mati(mati):
     fila, columna = len( mati ), len( mati[0] )
-    for i in range(fila):
+    for tamaño in range(fila):
         new = []
         for j in range(columna):
-            new.append( [( lc.modulo( mati[i][j] )**2 ),0])   
-        mati[i]= new
+            new.append( [( lc.modulo( mati[tamaño][j] )**2 ),0])   
+        mati[tamaño]= new
     return mati
 
 
@@ -20,7 +20,7 @@ def sistemaprobabilistico_comple(mati, vec, veces):
         tamaño  = len(vec)
         #copia matriz
         igual = mati[:]
-        for i in range(veces ):
+        for tamaño in range(veces ):
             mati = multiplicacion(mati, igual)
         return ult_mati(mati)
     return -1
@@ -29,7 +29,7 @@ def sistemaprobabilistico_comple(mati, vec, veces):
 def sistemaprobabilistico(mati, vec, veces):
     if (veces >= 0) and (type(veces) is int):
         tamaño = len(vec)
-        for i in range(veces):
+        for tamaño in range(veces):
             vec = accion(mati, vec)
         return vec
     return -1
@@ -42,17 +42,33 @@ def experi_bolean_mat(veces,mat,vec):
         return vec
 
 def multipleslitexperiment_cuantico(mati , vec, veces ):
-    return sistemaprobabilistico_comple(mati , vec, veces )       
+    return sistemaprobabilistico_comple(mati , vec, veces ) 
+
 
 def multipleslitexperimento(mati,vec,veces):
         return  sistemaprobabilistico(mati,vec,veces)
     
 
 def grafico(vector):
-    data = len(vector)
-    x = np.array([ x for x in range( data )])
-    y = np.array([ round(vector[x][0]*100,2) for x in range( data )])
+    tamaño = len(vector)
+    x = np.array([x for x in range(tamaño)])
+    y = np.array([round(vector[x][0]*100,2) for x in range(tamaño)])
 
-    plot.bar(x,y, color ='g', align='centro')
+    plot.bar(x,y, color ='g', align='center')
     plot.title('Probabilidades del vector')
     plot.show()
+
+    
+matriz_doble_rendija = [[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+           [[1 / math.sqrt(2), 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+           [[1 / math.sqrt(2), 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+           [[0, 0], [-1 / math.sqrt(6), 1 / math.sqrt(6)], [0, 0], [1, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+           [[0, 0], [-1 / math.sqrt(6), -1 / math.sqrt(6)], [0, 0], [0, 0], [1, 0], [0, 0], [0, 0], [0, 0]],
+           [[0, 0], [1 / math.sqrt(6), -1 / math.sqrt(6)], [-1 / math.sqrt(6), 1 / math.sqrt(6)], [0, 0], [0, 0],[1, 0], [0, 0], [0, 0]],
+           [[0, 0], [0, 0], [-1 / math.sqrt(6), -1 / math.sqrt(6)], [0, 0], [0, 0], [0, 0], [1, 0], [0, 0]],
+           [[0, 0], [0, 0], [1 / math.sqrt(6), -1 / math.sqrt(6)], [0, 0], [0, 0], [0, 0], [0, 0], [1, 0]]]
+       
+estado_inicial = [[1, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+
+res = accion(multipleslitexperiment_cuantico(matriz_doble_rendija[:],estado_inicial[:], 2),  estado_inicial)
+print(grafico( res))
